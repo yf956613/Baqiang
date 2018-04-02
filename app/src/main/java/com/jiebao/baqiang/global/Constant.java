7664a3811a74edf814414806f029e5e013fda5d7
ZHANGfeng-james <zfengwust3054@163.com>
2018-04-02 20:58:36 +0800

    关闭Debug

--- app/src/main/java/com/jiebao/baqiang/global/Constant.java
+++ app/src/main/java/com/jiebao/baqiang/global/Constant.java
@@ -2,7 +2,7 @@ package com.jiebao.baqiang.global;
 
 public class Constant {
     // 调试阶段开关
-    public final static boolean DEBUG = true;
+    public final static boolean DEBUG = false;
 
     // 连续扫描延时，单位：ms
     public final static int TIME_SCAN_DELAY = 5000;
@@ -137,47 +137,72 @@ public class Constant {
     public static final int VEHICEINFO_ID = BASE_INFO_ID + 5;
 
     public static final int STARTDOWNLOAD_INFO = 1000;
-    public static final int STARTDOWNLOAD_SERVERINFO = STARTDOWNLOAD_INFO + SERVER_INFO_ID;
-    public static final int STARTDOWNLOAD_SALESINFO = STARTDOWNLOAD_INFO + SALESINFO_ID;
-    public static final int STARTDOWNLOAD_SHIPMENTTYPEINFO = STARTDOWNLOAD_INFO +
+    public static final int STARTDOWNLOAD_SERVERINFO = STARTDOWNLOAD_INFO +
+            SERVER_INFO_ID;
+    public static final int STARTDOWNLOAD_SALESINFO = STARTDOWNLOAD_INFO +
+            SALESINFO_ID;
+    public static final int STARTDOWNLOAD_SHIPMENTTYPEINFO =
+            STARTDOWNLOAD_INFO +
             SHIPMENTTYPEINFO_ID;
-    public static final int STARTDOWNLOAD_LIUCANGTYPEINFO = STARTDOWNLOAD_INFO + LIUCANGTYPEINFO_ID;
-    public static final int STARTDOWNLOAD_VEHICEINFO = STARTDOWNLOAD_INFO + VEHICEINFO_ID;
+    public static final int STARTDOWNLOAD_LIUCANGTYPEINFO =
+            STARTDOWNLOAD_INFO + LIUCANGTYPEINFO_ID;
+    public static final int STARTDOWNLOAD_VEHICEINFO = STARTDOWNLOAD_INFO +
+            VEHICEINFO_ID;
 
     public static final int DOWNLOAD_INFO_SUCCESS = 2000;
-    public static final int DOWNLOAD_SERVERINFO_SUCCESS = DOWNLOAD_INFO_SUCCESS + SERVER_INFO_ID;
-    public static final int DOWNLOAD_SALESINFO_SUCCESS = DOWNLOAD_INFO_SUCCESS + SALESINFO_ID;
-    public static final int DOWNLOAD_SHIPMENTTYPEINFO_SUCCESS = DOWNLOAD_INFO_SUCCESS +
+    public static final int DOWNLOAD_SERVERINFO_SUCCESS =
+            DOWNLOAD_INFO_SUCCESS + SERVER_INFO_ID;
+    public static final int DOWNLOAD_SALESINFO_SUCCESS =
+            DOWNLOAD_INFO_SUCCESS + SALESINFO_ID;
+    public static final int DOWNLOAD_SHIPMENTTYPEINFO_SUCCESS =
+            DOWNLOAD_INFO_SUCCESS +
             SHIPMENTTYPEINFO_ID;
-    public static final int DOWNLOAD_LIUCANGTYPEINFO_SUCCESS = DOWNLOAD_INFO_SUCCESS +
+    public static final int DOWNLOAD_LIUCANGTYPEINFO_SUCCESS =
+            DOWNLOAD_INFO_SUCCESS +
             LIUCANGTYPEINFO_ID;
-    public static final int DOWNLOAD_VEHICEINFO_SUCCESS = DOWNLOAD_INFO_SUCCESS + VEHICEINFO_ID;
+    public static final int DOWNLOAD_VEHICEINFO_SUCCESS =
+            DOWNLOAD_INFO_SUCCESS + VEHICEINFO_ID;
 
 
     public static final int DOWNLOAD_INFO_FAILED = 3000;
-    public static final int DOWNLOAD_SERVERINFO_FAILED = DOWNLOAD_INFO_FAILED + SERVER_INFO_ID;
-    public static final int DOWNLOAD_SALESINFO_FAILED = DOWNLOAD_INFO_FAILED + SALESINFO_ID;
-    public static final int DOWNLOAD_SHIPMENTTYPEINFO_FAILED = DOWNLOAD_INFO_FAILED +
+    public static final int DOWNLOAD_SERVERINFO_FAILED = DOWNLOAD_INFO_FAILED
+            + SERVER_INFO_ID;
+    public static final int DOWNLOAD_SALESINFO_FAILED = DOWNLOAD_INFO_FAILED
+            + SALESINFO_ID;
+    public static final int DOWNLOAD_SHIPMENTTYPEINFO_FAILED =
+            DOWNLOAD_INFO_FAILED +
             SHIPMENTTYPEINFO_ID;
-    public static final int DOWNLOAD_LIUCANGTYPEINFO_FAILED = DOWNLOAD_INFO_FAILED +
+    public static final int DOWNLOAD_LIUCANGTYPEINFO_FAILED =
+            DOWNLOAD_INFO_FAILED +
             LIUCANGTYPEINFO_ID;
-    public static final int DOWNLOAD_VEHICEINFO_FAILED = DOWNLOAD_INFO_FAILED + VEHICEINFO_ID;
+    public static final int DOWNLOAD_VEHICEINFO_FAILED = DOWNLOAD_INFO_FAILED
+            + VEHICEINFO_ID;
 
 
     public static final int UPDATE_DATA_SUCCESS = 4000;
-    public static final int UPDATE_SERVERINFO_SUCCESS = UPDATE_DATA_SUCCESS + SERVER_INFO_ID;
-    public static final int UPDATE_SALESINFO_SUCCESS = UPDATE_DATA_SUCCESS + SALESINFO_ID;
-    public static final int UPDATE_SHIPMENTTYPEINFO_SUCCESS = UPDATE_DATA_SUCCESS + SHIPMENTTYPEINFO_ID;
-    public static final int UPDATE_LIUCANGTYPEINFO_SUCCESS = UPDATE_DATA_SUCCESS + LIUCANGTYPEINFO_ID;
-    public static final int UPDATE_VEHICEINFO_SUCCESS = UPDATE_DATA_SUCCESS + VEHICEINFO_ID;
+    public static final int UPDATE_SERVERINFO_SUCCESS = UPDATE_DATA_SUCCESS +
+            SERVER_INFO_ID;
+    public static final int UPDATE_SALESINFO_SUCCESS = UPDATE_DATA_SUCCESS +
+            SALESINFO_ID;
+    public static final int UPDATE_SHIPMENTTYPEINFO_SUCCESS =
+            UPDATE_DATA_SUCCESS + SHIPMENTTYPEINFO_ID;
+    public static final int UPDATE_LIUCANGTYPEINFO_SUCCESS =
+            UPDATE_DATA_SUCCESS + LIUCANGTYPEINFO_ID;
+    public static final int UPDATE_VEHICEINFO_SUCCESS = UPDATE_DATA_SUCCESS +
+            VEHICEINFO_ID;
 
     public static final int UPDATE_DATA_FAILED = 5000;
-    public static final int UPDATE_SEVERINFO_FAILED = UPDATE_DATA_FAILED + SERVER_INFO_ID;
-    public static final int UPDATE_SALESINFO_FAILED = UPDATE_DATA_FAILED + SALESINFO_ID;
-    public static final int UPDATE_SHIPMENTTYPEINFO_FAILED = UPDATE_DATA_FAILED +
+    public static final int UPDATE_SEVERINFO_FAILED = UPDATE_DATA_FAILED +
+            SERVER_INFO_ID;
+    public static final int UPDATE_SALESINFO_FAILED = UPDATE_DATA_FAILED +
+            SALESINFO_ID;
+    public static final int UPDATE_SHIPMENTTYPEINFO_FAILED =
+            UPDATE_DATA_FAILED +
             SHIPMENTTYPEINFO_ID;
-    public static final int UPDATE_LIUCANGTYPEINFO_FAILED = UPDATE_DATA_FAILED + LIUCANGTYPEINFO_ID;
-    public static final int UPDATE_VEHICEINFO_FAILED = UPDATE_DATA_FAILED + VEHICEINFO_ID;
+    public static final int UPDATE_LIUCANGTYPEINFO_FAILED =
+            UPDATE_DATA_FAILED + LIUCANGTYPEINFO_ID;
+    public static final int UPDATE_VEHICEINFO_FAILED = UPDATE_DATA_FAILED +
+            VEHICEINFO_ID;
 
 
     public static final String ARG_USE_CAMERA = "useCamera";
